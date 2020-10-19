4e8c59940aad543c3563c28bc45845af713fa357
18919656953@163.com <qidongliang@variflight.com>
2020-10-19 10:34:59 +0800

    Initial commit

---
+++ src/main/java/com/hfcsbc/operation/carlife/order/job/OperationCarlifeOrderJobApplication.java
@@ -0,0 +1,13 @@
+package com.hfcsbc.operation.carlife.order.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationCarlifeOrderJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperationCarlifeOrderJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hfcsbc/operation/carlife/order/job/OperationCarlifeOrderJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfcsbc.operation.carlife.order.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationCarlifeOrderJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
