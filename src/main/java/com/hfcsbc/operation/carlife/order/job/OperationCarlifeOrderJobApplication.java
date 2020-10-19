359ffd8a692e1358344be050e7edf9582f679403
18919656953@163.com <qidongliang@variflight.com>
2020-10-19 11:40:42 +0800

    运行环境配置

--- src/main/java/com/hfcsbc/operation/carlife/order/job/OperationCarlifeOrderJobApplication.java
+++ src/main/java/com/hfcsbc/operation/carlife/order/job/OperationCarlifeOrderJobApplication.java
@@ -1,13 +1,27 @@
 package com.hfcsbc.operation.carlife.order.job;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+@Slf4j
+@ComponentScan(value = "com.hfcsbc.*")
+@EnableFeignClients
 @SpringBootApplication
 public class OperationCarlifeOrderJobApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OperationCarlifeOrderJobApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        log.info("---娃哈哈---");
+        return null;
     }
 
 }
